7f24a032e7d88042733c8e4461dcf8d3815eba14
Stageddat <chmarc72@gmail.com>
2026-03-28 02:01:28 +0100

    docs: add Tresor Constructor Javadoc comment

--- src/Clases/Tresor.java
+++ src/Clases/Tresor.java
@@ -4,18 +4,23 @@ public class Tresor {
 	private String nom;
 	private int valor;
 	private double pes;
+
+	/**
+	 * Constructor de la classe Tresor
+	 * 
+	 * @param nom   Nom del tresor
+	 * @param valor Valor del tresor
+	 * @param pes   Pes del tresor
+	 */
 	public Tresor(String nom, int valor, double pes) {
 		super();
 		this.nom = nom;
 		this.valor = valor;
 		this.pes = pes;
 	}
-	
+
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "Nom: " + nom + " | Valor: " + valor;
 	}
-	
-	
 }
