1b3a8d675859f32bde8cf3bdbe160e8f947dc26f
Stageddat <chmarc72@gmail.com>
2026-03-29 21:18:54 +0200

    feat: implement setters and getters to Tresor

--- src/model/Tresor.java
+++ src/model/Tresor.java
@@ -1,22 +1,48 @@
 package model;
 
+/**
+ * Representació d'un Tresor.
+ */
 public class Tresor {
 	private String nom;
 	private int valor;
 	private double pes;
 
-	/**
-	 * Constructor de la classe Tresor
-	 * 
-	 * @param nom   Nom del tresor
-	 * @param valor Valor del tresor
-	 * @param pes   Pes del tresor
-	 */
 	public Tresor(String nom, int valor, double pes) {
-		super();
-		this.nom = nom;
-		this.valor = valor;
-		this.pes = pes;
+		// Utilitzar setters ja que tenen validacions
+		setNom(nom);
+		setValor(valor);
+		setPes(pes);
+	}
+
+	// Getters
+	public String getNom() {
+		return nom;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public double getPes() {
+		return pes;
+	}
+
+	// Setters
+	public void setNom(String nom) {
+		if (nom == null || nom.isEmpty()) {
+			this.nom = "Default";
+		} else {
+			this.nom = nom;
+		}
+	}
+
+	public void setValor(int valor) {
+		this.valor = (valor > 0) ? valor : 0;
+	}
+
+	public void setPes(double pes) {
+		this.pes = (pes > 0) ? pes : 0;
 	}
 
 	@Override
