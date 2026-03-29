bae9e9ae69960a249743567752d4c5b5174fca1c
Stageddat <chmarc72@gmail.com>
2026-03-30 00:53:26 +0200

    feat: implement random numer generator

--- src/utils/MathUtils.java
+++ src/utils/MathUtils.java
@@ -18,4 +18,15 @@ public class MathUtils {
 	public static int ajustarRang(int min, int max, int valor) {
 		return Math.min(Math.max(min, valor), max);
 	}
+
+	/**
+	 * Genera un número aleatori sencer entre el valor mínim i el màxim.
+	 * 
+	 * @param min El valor límit inferior.
+	 * @param max El valor límit superior.
+	 * @return El número aleatori generat.
+	 */
+	public static int generarNumeroAleatori(int min, int max) {
+		return (int) (Math.random() * (max - min + 1)) + min;
+	}
 }
