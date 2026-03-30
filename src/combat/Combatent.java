1e3b8a2b1ee5b0bfae4efdbe85da3d3c734619cc
Stageddat <chmarc72@gmail.com>
2026-03-30 23:01:52 +0200

    feat: implement function in Combatent interface to check if can fight

--- src/combat/Combatent.java
+++ src/combat/Combatent.java
@@ -7,12 +7,21 @@ public interface Combatent {
 
 	int calcularAtac();
 
-	default void rebreDany(int quantitat) {
-		int nouVida = getVida() - quantitat;
-		setVida(Math.max(0, nouVida));
-	};
+	boolean estaViu();
 
-	default boolean estaViu() {
-		return getVida() > 0;
+	default void rebreDany(int quantitat) {
+		setVida(Math.max(0, getVida() - quantitat));
+	}
+
+	/**
+	 * Comprova si el combatent pot lluitar.
+	 * Es pot lluitar en cas que el combatent estigui viu i el openent també estigui
+	 * viu.
+	 * 
+	 * @param oponent El combatent oponent.
+	 * @return El combatent pot lluitar o no.
+	 */
+	default boolean potLluitar(Combatent oponent) {
+		return this.estaViu() && oponent != null && oponent.estaViu();
 	}
 }
