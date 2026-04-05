1000452efd7398b7223832133f440d7ca4c41df4
Stageddat <chmarc72@gmail.com>
2026-04-06 01:54:30 +0200

    feat: implement dungeon generation and display logic in Masmorra class

--- src/Masmorra.java
+++ src/Masmorra.java
@@ -1,85 +1,100 @@
 
 import java.util.Scanner;
 
+import model.Monstre;
 import model.Personatge;
+import model.Tresor;
+import sala.Sala;
+import sala.SalaComuna;
+import sala.SalaPont;
+import sala.SalaTeranyina;
 
 public class Masmorra {
+	// Matriu sala
+	private int x;
+	private int y;
+	Sala[][] mapa = new Sala[x][y];
+	private int salesExplorades = 0;
+	private int salesTotals = x * y;
 
-	public static void tauler() {
-		// Valor provisional
-		int x = 10;
-		int y = 10;
-		char[][] matriu = new char[x][y];
+	// Personatge
+	Personatge personatge;
 
-		for (int fil = 0; fil < matriu.length; fil++) {
-			for (int col = 0; col < matriu[fil].length; col++) {
+	// Tresors posibles
+	private static final Tresor[] LLISTA_TRESORS = {
+			new Tresor("Copa d'Or", 100, 2.5),
+			new Tresor("Anell de Plata", 50, 0.5),
+			new Tresor("Diamant Brut", 500, 1.0)
+	};
 
-				// S'HA ALEATORITZA EL TAULER//
-				int random = (int) (Math.random() * 100) + 1;
-				if (random <= 20) {
-					matriu[fil][col] = 'P';// Sala pont
-				} else if (random > 20 && random <= 40) {
-					matriu[fil][col] = 'T';// Sala teranyina
+	// Monstres posibles
+	private static final Monstre[] LLISTA_MONSTRES = {
+			new Monstre("Zombie", 10, 2),
+			new Monstre("Aranya", 5, 1),
+			new Monstre("Esquelet", 8, 3)
+	};
+
+	public Masmorra(int x, int y, Personatge personatge) {
+		this.x = x;
+		this.y = y;
+		this.mapa = generarMasmorra();
+		this.salesExplorades = salesExplorades;
+		this.salesTotals = salesTotals;
+		this.personatge = personatge;
+	}
+
+	private Tresor generarTresor() {
+		if (Math.random() < 0.30) {
+			Tresor plantillaTresor = LLISTA_TRESORS[(int) (Math.random() * LLISTA_TRESORS.length)];
+
+			return new Tresor(plantillaTresor.getNom(), plantillaTresor.getValor(), plantillaTresor.getPes());
+		}
+		return null;
+	}
+
+	private Monstre generarMonstre() {
+		if (Math.random() < 0.40) {
+			Monstre plantillaMonstre = LLISTA_MONSTRES[(int) (Math.random() * LLISTA_MONSTRES.length)];
+
+			return new Monstre(plantillaMonstre.getNom(), plantillaMonstre.getVida(), plantillaMonstre.getPenalitzacio());
+		}
+		return null;
+	}
+
+	public Sala[][] generarMasmorra() {
+		Sala[][] nouMapa = new Sala[this.x][this.y];
+		for (int i = 0; i < this.x; i++) {
+			for (int j = 0; j < this.y; j++) {
+				double r = Math.random();
+
+				// 60 sala comuna
+				// 20 sala pont
+				// 20 sala teranyina
+				if (r < 0.60) {
+					nouMapa[i][j] = new SalaComuna(generarTresor(), generarMonstre(), false);
+				} else if (r < 0.80) {
+					nouMapa[i][j] = new SalaPont(generarTresor(), generarMonstre(), false, personatge);
 				} else {
-					matriu[fil][col] = 'C';// Sala comuna
+					nouMapa[i][j] = new SalaTeranyina(generarTresor(), generarMonstre(), false, personatge);
 				}
+			}
+		}
+		return nouMapa;
+	}
 
-				// CREEACIÓ DE LA TAULA//
-				System.out.print(matriu[fil][col] + "   | ");
+	public void mostrarMasmorra(Personatge personatge) {
+		for (int i = 0; i < this.x; i++) {
+			for (int j = 0; j < this.y; j++) {
+				int[] posicioPersonatge = personatge.getPosicio();
+				if (posicioPersonatge[0] == i && posicioPersonatge[1] == j) {
+					System.out.print("[ & ]");
+				} else if (mapa[i][j].estaExplorada()) {
+					System.out.print("[ * ]");
+				} else {
+					System.out.print("[ - ]");
+				}
 			}
 			System.out.println();
 		}
 	}
-
-	public static void opcions(Personatge p) {
-		Scanner teclado = new Scanner(System.in);
-		if () {//Si hay un monstruo
-			System.out.println("Que vols fer Explorar (E) / Atacar (A) / Moure (M)");
-			String opcio = teclado.nextLine();
-			if (opcio.equalsIgnoreCase("e") || opcio.equalsIgnoreCase("explorar")) {
-				
-			}else if (opcio.equalsIgnoreCase("a") || opcio.equalsIgnoreCase("atacar")) {
-			
-			}else if (opcio.equalsIgnoreCase("m") || opcio.equalsIgnoreCase("moure")) {
-				System.out.print("Direcció del moviment: ");
-				char direccio = teclado.next().charAt(0);
-				p.moure(direccio);
-			}else {
-				System.out.println("Opció no vàlida");
-			}
-		}else {//Si no hay un monstruo
-		System.out.println("Que vols fer Explorar (E) / Moure (M)");
-		String opcio = teclado.nextLine();
-		if (opcio.equalsIgnoreCase("e") || opcio.equalsIgnoreCase("explorar")) {
-			
-		}else if (opcio.equalsIgnoreCase("m") || opcio.equalsIgnoreCase("moure")) {
-			System.out.print("Direcció del moviment: ");
-			char direccio = teclado.next().charAt(0);
-			p.moure(direccio);
-		}else {
-			System.out.println("Opció no vàlida");
-		}
-		}
-	}
-
-	public static void finalitzar(Personatge p) {
-		boolean finalitzar = false;
-		if (finalitzar) {
-			System.out.println("");
-		} else {
-			System.out.println("Encara no s'ha finalitzat");
-			if (p.vida == 0) {
-				finalitzar = true;
-				System.out.println("Partida Fallida");
-				System.out.println("Experiencia obtenida: " + p.experiencia);
-				if (montre) {// Causa de la mort MONSTRE
-
-				} else {// Causa de la mort PONT // TIRADES DE DAU
-
-				}
-				System.out.println("Percentatge explorat: " + explPer);
-			}
-		}
-	}
-
 }
