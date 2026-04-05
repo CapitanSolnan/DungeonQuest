43281133a5606b81840979ada979051ce1cccc04
Stageddat <chmarc72@gmail.com>
2026-04-05 21:37:20 +0200

    refactor: rename parameters for clarity in SalaComuna constructor

--- src/sala/SalaComuna.java
+++ src/sala/SalaComuna.java
@@ -4,13 +4,12 @@ import model.Monstre;
 import model.Tresor;
 
 public class SalaComuna extends Sala {
-
-	public SalaComuna(Tresor t, Monstre m, boolean explorada) {
-		super(t, m, explorada);
+	public SalaComuna(Tresor tresor, Monstre monstre, boolean explorada) {
+		super(tresor, monstre, explorada);
 	}
 
+	@Override
 	public boolean intentarSortir() {
 		return true;
 	}
-
 }
