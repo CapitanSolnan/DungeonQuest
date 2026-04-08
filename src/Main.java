737352df1aeec26d470f59501e5cb6bdae9bcfaf
Stageddat <chmarc72@gmail.com>
2026-04-08 18:16:26 +0200

    fix: update colors in welcome msg

--- src/Main.java
+++ src/Main.java
@@ -60,13 +60,13 @@ public class Main {
 	}
 
 	private static void imprimirMissatgeBenvinguda(Scanner teclado) {
-		System.out.println(Colors.CIAN_B);
+		System.out.println(Colors.CIAN);
 		System.out.println(" ____  _   _ _   _  ____ _____ ___  _   _    ___  _   _ _____ ____ _____ ");
 		System.out.println("|  _ \\| | | | \\ | |/ ___| ____/ _ \\| \\ | |  / _ \\| | | | ____/ ___|_   _|");
 		System.out.println("| | | | | | |  \\| | |  _|  _|| | | |  \\| | | | | | | | |  _| \\___ \\ | |  ");
 		System.out.println("| |_| | |_| | |\\  | |_| | |__| |_| | |\\  | | |_| | |_| | |___ ___) || |  ");
 		System.out.println("|____/ \\___/|_| \\_|\\____|_____\\___/|_| \\_|  \\__\\_\\\\___/|_____|____/ |_|  ");
-		System.out.println(Colors.VERD_B + "Fet per: Javi i Marc");
+		System.out.println(Colors.VERD + "Fet per: Javi i Marc");
 		System.out.println("DAM 1M - Programació");
 		System.out.println("CURS 2025-2026");
 		System.out.println(Colors.RESET);
