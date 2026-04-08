4c98ad27477473dc1c570703cd053f6e6baa7002
Stageddat <chmarc72@gmail.com>
2026-04-08 10:23:09 +0200

    refactor: optimize console clearing method in saltarPagina

--- src/Main.java
+++ src/Main.java
@@ -135,15 +135,19 @@ public class Main {
 
 	// funcions per netejar consola
 	private static void saltarPagina() {
-		for (int i = 0; i < 50; i++) {
-			System.out.println();
-		}
+		// for (int i = 0; i < 50; i++) {
+		// System.out.println();
+		// }
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 	}
 
 	private static void saltarPagina(String titol) {
-		for (int i = 0; i < 50; i++) {
-			System.out.println();
-		}
+		// for (int i = 0; i < 50; i++) {
+		// System.out.println();
+		// }
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 		System.out.println(titol);
 	}
 }
